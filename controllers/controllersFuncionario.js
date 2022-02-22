2e35f1de2eba491c8e75095dc3d30991258d0e97
guisantosalves <workguisantos@gmail.com>
2022-02-22 10:43:23 -0400

    Merge branch 'master' of https://github.com/guisantosalves/api_v1_mecanica

--- controllers/controllersFuncionario.js
+++ controllers/controllersFuncionario.js
@@ -1,35 +1,76 @@
 const database = require('../connection/db');
 const modelFunc = require('../model/modelFuncionario');
+
 (async () => {
-    
     try {
         const resultado = await database.sync();
-        //console.log(resultado);
     } catch (error) {
         console.log(error);
     }
 })();
 
 module.exports.getFuncionario = async (req, res)=>{
-    const funcionario = await modelFunc.findAll();
-    //console.log(funcionario);
-    res.send(funcionario);
+    try{
+        const funcionario = await modelFunc.findAll();
+        res.status(200);
+        res.send(funcionario);
+    }catch(err){
+        res.status(400).json({erro: err});
+    }
 }
 
 module.exports.postFuncionario = async (req, res)=>{
+    try{
+        var nome = req.body.nome_funcionario;
+        var cpf = req.body.cpf_funcionario;
+        var telefone = req.body.telefone;
+
+        const resultadoCreate = await modelFunc.create({
+            nome_funcionario: nome,
+            cpf_funcionario: cpf,
+            telefone: telefone
+        })
+        res.status(200);
+        res.send(resultadoCreate);
+    }catch(err){
+        res.status(400).json({erro: err})
+    }
+}
+
+module.exports.putFuncionario = async (req, res)=>{
+    try{
+        var nome = req.body.nome_funcionario;
+        var cpf = req.body.cpf_funcionario;
+        var telefone = req.body.telefone;
+        var id = req.body.id;
+
+        const resultadoUpdate = await modelFunc.update({nome_funcionario: nome, cpf_funcionario: cpf, telefone: telefone},
+        {where: {id_funcionario: id}});
+        
+        if(resultadoUpdate == 1){
+            res.status(200);
+            res.send('Registro alterado com sucesso');
+        }else{
+            res.send('Algo inesperado aconteceu');
+        }
+    }catch(err){
+        res.status(400).json({error: err});
+    }
+}
+
+module.exports.deleteFuncionario = async (req, res)=>{
+
+    try{
+        var id = req.body.id;
+
+        const resultadoDelete = await modelFunc.destroy({where: {
+            id_funcionario: id
+        }}).then((a)=>{
+            res.send('deletado com sucesso');
+            res.status(200);
+        });
+    }catch(err){
+        res.status(400).json({erro: err});
+    }
     
-    const resultadoCreate = await modelFunc.create({
-        nome_funcionario: 'guizaoo',
-        cpf_funcionario: '10123135645',
-        telefone: '123456789'
-    })
-    res.send(resultadoCreate);
-}
-
-module.exports.putFuncionario = (req, res)=>{
-    res.send('vc está no update funcionario')
-}
-
-module.exports.deleteFuncionario = (req, res)=>{
-    res.send('vc está no delete funcionario');
 }
